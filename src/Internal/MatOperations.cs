2cca47eac7c79f0a68d664329d24718c351d3631
Silio Leonardo (CtP/ETS) <sii5ct@bosch.com>
2023-06-06 10:13:38 -0300

    Added more functions of sum operation for many cases

--- src/Internal/MatOperations.cs
+++ src/Internal/MatOperations.cs
@@ -8,8 +8,11 @@ namespace Algebric.Internal;
 internal static class MatOperations
 {
     private const int splitThreshold = 128;
-    
-    internal static unsafe void Sum(float* p, float* q, float n, float m)
+
+    internal static unsafe void Sum(
+        float* p, float* q,
+        int n, int m
+    )
     {
         if (p is null)
             throw new ArgumentNullException("p");
@@ -20,29 +23,138 @@ internal static class MatOperations
         if (n < 1 || m < 1)
             throw new InvalidOperationException("size of matriz may be bigger than 0");
         
-        if (n == m) sum(p, q, n);
+        if (n == m)
+            sum(p, q, n);
         else sum(p, q, n, m);
     }
     
-    internal static unsafe void sum(float* p, float* q, float n, float m)
+    internal static unsafe void Sum(
+        float* p, float* q, 
+        int pi, int pj,
+        int qi, int qj,
+        int n, int m
+    )
+    {
+        if (p is null)
+            throw new ArgumentNullException("p");
+        
+        if (q is null)
+            throw new ArgumentNullException("q");
+        
+        if (n < 1 || m < 1)
+            throw new InvalidOperationException("size of matriz may be bigger than 0");
+        
+        if (n == m) 
+            sum(p, q, pi, pj, qi, qj, n);
+        else sum(p, q, pi, pj, qi, qj, n, m);
+    }
+
+    internal static unsafe void sum(
+        float* p, float* q,
+        int pi, int pj,
+        int qi, int qj,
+        int n, int m
+    )
     {
         
     }
 
-    internal static unsafe void sum(float* p, float* q, float n)
+    internal static unsafe void sum(
+        float* p, float* q,
+        int pi, int pj,
+        int qi, int qj,
+        int n
+    )
     {
-        if (n < splitThreshold || Environment.ProcessorCount < 2)
+        
+    }
+    
+    internal static unsafe void sum(
+        float* p, float* q, 
+        int n, int m
+    )
+    {
+        
+    }
+
+    internal static unsafe void sum(
+        float* p, float* q,
+        int n
+    )
+    {
+        if (n < splitThreshold)
             iterativeSum(p, q, n);
         else parallelSum(p, q, n);
     }
 
-    internal static unsafe void iterativeSum(float* p, float* q, float n)
+    internal static unsafe void iterativeSum(
+        float* p, float* q,
+        int n
+    )
     {
         
     }
 
-    internal static unsafe void parallelSum(float* p, float* q, float n)
+    internal static unsafe void parallelSum(
+        float* p, float* q, 
+        int n
+    )
     {
 
+    }
+
+    internal static unsafe void iterativeSum(
+        float* p, float* q,
+        int n, int m
+    )
+    {
+        
+    }
+
+    internal static unsafe void parallelSum(
+        float* p, float* q, 
+        int n, int m
+    )
+    {
+
+    }
+    internal static unsafe void iterativeSum(
+        float* p, float* q,
+        int pi, int pj,
+        int qi, int qj,
+        int n, int m
+    )
+    {
+        
+    }
+
+    internal static unsafe void parallelSum(
+        float* p, float* q,
+        int pi, int pj,
+        int qi, int qj,
+        int n
+    )
+    {
+
+    }
+
+    internal static unsafe void iterativeSum(
+        float* p, float* q,
+        int pi, int pj,
+        int qi, int qj,
+        int n
+    )
+    {
+        
+    }
+
+    internal static unsafe void parallelSum(
+        float* p, float* q,
+        int pi, int pj,
+        int qi, int qj,
+        int n, int m
+    )
+    {
+        
     }
 }
